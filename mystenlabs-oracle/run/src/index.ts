cc81fea050af1929f1973ebb57178f43d186de99
“George <george.digkas@mystenlabs.com>
2023-10-06 13:17:51 +0300

    cleanup

--- mystenlabs-oracle/run/src/index.ts
+++ mystenlabs-oracle/run/src/index.ts
@@ -1,3 +1,4 @@
+// Import the necessary modules from @mysten/sui.js
 import {
   Connection,
   Ed25519Keypair,
@@ -5,42 +6,55 @@ import {
   RawSigner,
   TransactionBlock,
 } from "@mysten/sui.js";
+
+// Import the dotenv module to load environment variables from a .env file
 import * as dotenv from "dotenv";
 
+// Load the .env file from the parent directory
 dotenv.config({ path: "../.env" });
 
+// Get the phrase and fullnode values from the environment variables
 const phrase = process.env.ADMIN_PHRASE;
 const fullnode = process.env.FULLNODE!;
+
+// Derive a keypair from the phrase using the Ed25519 algorithm
 const keypair = Ed25519Keypair.deriveKeypair(phrase!);
-const adminAddress = keypair.getPublicKey().toSuiAddress();
+
+// Create a provider that connects to the fullnode using JSON-RPC protocol
 const provider = new JsonRpcProvider(
   new Connection({
     fullnode: fullnode,
   })
 );
+
+// Create a signer that uses the keypair and the provider to sign transactions
 const signer = new RawSigner(keypair, provider);
 
+// Get the package IDs of the mystenlabs oracle and the demo app from the environment variables
 const mystenlabsOraclePackageId = process.env.MYSTENLABS_ORACLE_PACKAGE_ID;
 const mystenlabsOracleModuleName = "mystenlabs_oracle";
 
 const demoAppPackageId = process.env.DEMO_APP_PACKAGE_ID;
 const demoAppModuleName = "interact";
 
-const weather_oracle =
-  "0x1aedcca0b67b891c64ca113fce87f89835236b4c77294ba7e2db534ad49a58dc";
-
+// Create a new transaction block to store multiple transactions
 let transactionBlock = new TransactionBlock();
 
+// Call the authorize function from the mystenlabs oracle module and store the result in authorization
 const authorization = transactionBlock.moveCall({
   target: `${mystenlabsOraclePackageId}::${mystenlabsOracleModuleName}::authorize`,
 });
 
+// Call the interact function from the demo app module and pass the authorization as an argument
 transactionBlock.moveCall({
   target: `${demoAppPackageId}::${demoAppModuleName}::interact`,
   arguments: [authorization],
 });
 
+// Set the gas budget for the transaction block to 10 million units
 transactionBlock.setGasBudget(10000000);
+
+// Sign and execute the transaction block using the signer and wait for local execution
 signer
   .signAndExecuteTransactionBlock({
     transactionBlock,
@@ -51,5 +65,6 @@ signer
     },
   })
   .then((result) => {
+    // Log the result to the console
     console.log(result);
   });
